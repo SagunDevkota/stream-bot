ca5d26579369c7470d34638a2b216d9b5161373b
Sagun Devkota <55743561+SagunDevkota@users.noreply.github.com>
2021-10-22 21:45:29 +0545

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,27 +1,32 @@
 import requests
 import bs4
 def all_matches_name():
-    try:    
-        res = requests.get("https://buffersports.com/football-games")
-        soup = bs4.BeautifulSoup(res.text,'lxml').find_all('ul',attrs={"class":"country-sales list-group list-group-flush container"})[0]
+    try: 
+        res = requests.get("https://totalsportek.pro/")
 
-        all_match_name=[]
-        all_match_link=[]
+        soup = bs4.BeautifulSoup(res.text,'lxml').find_all('div',attrs={"class":"div-main-box"})
 
-        for list_group_item in soup.find_all('li',attrs={"class":"country-sales-content list-group-item"}):
-            link_counter = 0
-            for match_name in list_group_item.find_all('span',attrs={"class":"ml-3 mt-4 col text-center"}):
-                match = match_name.find_all('a')[0].find_all('h6')[0]
-                accepted_str=""
-                for char in match.text:
-                    if((ord(char)>=65 and ord(char)<=90) or (ord(char)>=92 and ord(char)<=122)):
-                        accepted_str = accepted_str+char
-                all_match_name.append("/"+(accepted_str).replace(" ",""))
-            for match_link in list_group_item.find_all('span',attrs={"class":"text-dark col text-center"}):
-                link = match_link.find_all("a")
-                all_match_link.append(link[0]['href'])
-                link_counter+=1
+        all_match_link = []
+        all_match_name = []
+        for league in soup:
+            links = league.find_all("a")
+            for link in links:
+                temp = link.find_all("span",attrs={"class":"mt-2"})
+                home = ""
+                away = ""
+                for chars in temp[0].contents[0].upper():
+                    if(ord(chars)>=65 and ord(chars)<=90):
+                        home+=chars
+                for chars in temp[1].contents[0].upper():
+                    if(ord(chars)>=65 and ord(chars)<=90):
+                        away+=chars
+                all_match_name.append((f'/{home}vs{away}').replace(" ",""))
+                all_match_link.append(link['href'])
+
+
         all_matches = dict(zip(all_match_name,all_match_link))
+
+
         return all_matches
     except:
         return None
@@ -29,20 +34,11 @@ def all_matches_name():
 def selected_match(all_matches,user_match_name):
     try:
         users_match = requests.get(all_matches[user_match_name])
-        streaming_website = bs4.BeautifulSoup(users_match.text,'lxml').find_all('iframe')[0]['src']
 
-
-        streaming_document = requests.get(streaming_website)
-        streaming_link_list = bs4.BeautifulSoup(streaming_document.text,'lxml').find('table')
-        all_individual_match_link=[]
-
-        for links in streaming_link_list.find_all('tr'):
-            links_data = links.find_all('td')
-            for link in links_data:
-                x = link.find('a')
-                if(x!=None):
-                    all_individual_match_link.append(x['href'])
-
+        all_contents = bs4.BeautifulSoup(users_match.text,'lxml').find_all("tr",attrs={"class":"rounded-pill"})
+        all_individual_match_link = []
+        for content in all_contents:
+            all_individual_match_link.append(content.find_all("input")[0]['value'])
         return all_individual_match_link
     except:
         return None
--- main.py
+++ main.py
@@ -3,18 +3,34 @@ import bot
 from config import TOKEN,PORT
 
 def start(update,context):
-    update.message.reply_text("Welcome! to Football Story Bot")
+    id = update.message.chat.id
+    if(id == 753971038 or id == -1001331327568):
+        update.message.reply_text("Welcome! to Football Story Bot")
+    else:
+        update.message.reply_text("""
+            We have detected unusual request from your device.
+You have been banned from the service until further notice,
+        """)
 
 def help(update,context):
-    update.message.reply_text("""
-    The following commands are available:
-    /start -> Welcome Message
-    /help -> This Message
-    /streams -> list of matches
-    """)
+    id = update.message.chat.id
+    if(id == 753971038 or id == -1001331327568):
+        update.message.reply_text("""
+        The following commands are available:
+        /start -> Welcome Message
+        /help -> This Message
+        /streams -> list of matches
+        """)
+    else:
+        update.message.reply_text("""
+            We have detected unusual request from your device.
+You have been banned from the service until further notice,
+        """)
+
 
 def streams(update,context):
-    try:
+    id = update.message.chat.id
+    if(id == 753971038 or id == -1001331327568):
         update.message.reply_text("Searching for available matches")
         all_matches_name_list = bot.all_matches_name()
         link_str=''
@@ -25,11 +41,15 @@ def streams(update,context):
             link_str+=links+'\n'
         update.message.reply_text(link_str)
         update.message.reply_text("End Of Detected Matches")
-    except:
-        update.message.reply_text("We are experiencing problem.Try again later, if problem persists then contact admin")
+    else:
+        update.message.reply_text("""
+            We have detected unusual request from your device.
+You have been banned from the service until further notice,
+        """)
 
 def handle_message(update,context):
-    try:
+    id = update.message.chat.id
+    if(id == 753971038 or id == -1001331327568):
         update.message.reply_text("Searching for available streams")
         link_str=''
         sending_message=''
@@ -39,7 +59,7 @@ def handle_message(update,context):
             sending_message+=chars
         individual_link = bot.selected_match(bot.all_matches_name(),sending_message)
         if(individual_link==None):
-            update.message.reply_text("We are experiencing problem.Try again later, if problem persists then contact admin")
+            update.message.reply_text("We are experiencing problem.Try again later, if problem persists then contact admin hai tw")
             return None
         if(len(individual_link)==0):
             update.message.reply_text("No links found. Links are updated 30min before match. If link is not found till Kick Off then contact admin")
@@ -48,10 +68,11 @@ def handle_message(update,context):
             link_str+=links+'\n\n'
         update.message.reply_text(link_str)
         update.message.reply_text("End Of Streaming Links")
-    except:
-        update.message.reply_text("We are experiencing problem.Try again later, if problem persists then contact admin")
-
-
+    else:
+        update.message.reply_text("""
+            We have detected unusual request from your device.
+You have been banned from the service until further notice,
+        """)
 
 updater = telegram.ext.Updater(TOKEN,use_context=True)
 
