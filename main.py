064743e87a6074b27372e28797650e17e78442dd
Sagun Devkota <55743561+SagunDevkota@users.noreply.github.com>
2021-10-31 23:43:01 +0545

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def processing(msg):
     if 'text' in msg:
         for entry in regex:
             if re.findall(entry, msg["text"]):
-                matches = re.findall(entry, msg["text"])
+                matches = re.findall(entry, msg["text"]).groups()
                 parser(msg, matches)
                 return
 
@@ -66,48 +66,51 @@ regex = [
 def parser(msg, matches):
     usr = msg['from']
 
-    if msg['type'] == "text":
-        if matches[0] == 'start':
-            bot.sendMessage(usr['id'],"Welcome! to Football Story Bot")
-            return
+    if(usr['id']== 753971038 or usr[id] == -1001331327568):
+        if msg['type'] == "text":
+            if matches[0] == 'start':
+                bot.sendMessage(usr['id'],"Welcome! to Football Story Bot")
+                return
 
-        elif matches[0] == 'help':
-            bot.sendMessage(usr['id'], """
-        The following commands are available:
-        /start -> Welcome Message
-        /help -> This Message
-        /streams -> list of matches
-        """)
-            return
-        elif matches[0] == 'streams':
-            bot.sendMessage(usr['id'],"Searching for available matches")
-            all_matches_name_list = botTel.all_matches_name()
-            link_str=''
-            if(all_matches_name_list==None):
-                bot.sendMessage(usr['id'],"""We are experiencing problem.
-If problem persists then contact admin""")
-                return None
-            for links in all_matches_name_list:
-                link_str+=links+'\n'
-            bot.sendMessage(usr['id'],link_str)
-            bot.sendMessage(usr['id'],"End Of Detected Matches")
-
-        else:
-            if(len(matches)!=0):
-                bot.sendMessage("Searching for available streams")
+            elif matches[0] == 'help':
+                bot.sendMessage(usr['id'], """
+            The following commands are available:
+            /start -> Welcome Message
+            /help -> This Message
+            /streams -> list of matches
+            """)
+                return
+            elif matches[0] == 'streams':
+                bot.sendMessage(usr['id'],"Searching for available matches")
+                all_matches_name_list = botTel.all_matches_name()
                 link_str=''
-                sending_message=matches[0]
-                individual_link = bot.selected_mbot.sendMessageatch(botTel.all_matches_name(),sending_message)
-                if(individual_link==None):
-                    bot.sendMessage("We are experiencing problem.Try again later, if problem persists then contact admin hai tw")
+                if(all_matches_name_list==None):
+                    bot.sendMessage(usr['id'],"""We are experiencing problem.
+    If problem persists then contact admin""")
                     return None
-                if(len(individual_link)==0):
-                    bot.sendMessage("No links found. Links are updated 30min before match. If link is not found till Kick Off then contact admin")
-                    return None
-                for links in individual_link:
-                    link_str+=links+'\n\n'
-                bot.sendMessage(link_str)
-                bot.sendMessage("End Of Streaming Links")
+                for links in all_matches_name_list:
+                    link_str+=links+'\n'
+                bot.sendMessage(usr['id'],link_str)
+                bot.sendMessage(usr['id'],"End Of Detected Matches")
+
+            else:
+                if(len(matches)!=0):
+                    bot.sendMessage(usr['id'],"Searching for available streams")
+                    link_str=''
+                    sending_message=matches[0]
+                    individual_link = bot.selected_mbot.sendMessageatch(botTel.all_matches_name(),sending_message)
+                    if(individual_link==None):
+                        bot.sendMessage(usr['id'],"We are experiencing problem.Try again later, if problem persists then contact admin hai tw")
+                        return None
+                    if(len(individual_link)==0):
+                        bot.sendMessage(usr['id'],"No links found. Links are updated 30min before match. If link is not found till Kick Off then contact admin")
+                        return None
+                    for links in individual_link:
+                        link_str+=links+'\n\n'
+                    bot.sendMessage(usr['id'],link_str)
+                    bot.sendMessage(usr['id'],"End Of Streaming Links")
+        else:
+            bot.sendMessage(usr['id'],"You are banned.")
 
 if __name__ == "__main__":
     bot.setWebhook(URL, max_connections=10)
