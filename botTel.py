36b02aaace3e2b6cf18e9d5c485aabc8c0fcf00d
Sagun Devkota <55743561+SagunDevkota@users.noreply.github.com>
2021-10-31 22:10:04 +0545

    Add files via upload

---
+++ botTel.py
@@ -0,0 +1,48 @@
+import requests
+import bs4
+def all_matches_name():
+    try: 
+        res = requests.get("https://totalsportek.pro/")
+
+        soup = bs4.BeautifulSoup(res.text,'lxml').find_all('div',attrs={"class":"div-main-box"})
+
+        all_match_link = []
+        all_match_name = []
+        for league in soup:
+            links = league.find_all("a")
+            for link in links:
+                temp = link.find_all("span",attrs={"class":"mt-2"})
+                home = ""
+                away = ""
+                for chars in temp[0].contents[0].upper():
+                    if(ord(chars)>=65 and ord(chars)<=90):
+                        home+=chars
+                for chars in temp[1].contents[0].upper():
+                    if(ord(chars)>=65 and ord(chars)<=90):
+                        away+=chars
+                all_match_name.append((f'/{home}vs{away}').replace(" ",""))
+                all_match_link.append(link['href'])
+
+
+        all_matches = dict(zip(all_match_name,all_match_link))
+
+
+        return all_matches
+    except:
+        return None
+
+def selected_match(all_matches,user_match_name):
+    try:
+        all_links = 0
+        users_match = requests.get(all_matches[user_match_name])
+
+        all_contents = bs4.BeautifulSoup(users_match.text,'lxml').find_all("tr",attrs={"class":"rounded-pill"})
+        all_individual_match_link = []
+        for content in all_contents:
+            all_links = all_links+1
+            all_individual_match_link.append(content.find_all("input")[0]['value'])
+            if(all_links>=15):
+                break
+        return all_individual_match_link
+    except:
+        return None
--- main.py
+++ main.py
@@ -1,90 +1,97 @@
-import telegram.ext
-import bot
-from config import TOKEN,PORT
+import telepot
+import re
+import config
+import botTel
+from flask import Flask, render_template, session, url_for, redirect, request
+from telepot.namedtuple import *
 
+USERNAME = "hostingsd2"
+SECRET = 'abc'
+URL = f"https://{USERNAME}.pythonanywhere.com/{SECRET}"
 
+telepot.api.set_proxy('http://proxy.server:3128')
+bot = telepot.Bot(config.TOKEN)
+bot.setWebhook(URL, max_connections=10)
 
-def start(update,context):
-    id = update.message.chat.id
-    if(id == 753971038 or id == -1001331327568):
-        update.message.reply_text("Welcome! to Football Story Bot")
+def processing(msg):
+    if 'chat' in msg and msg['chat']['type'] == 'channel':
+        return
+        
+    id = msg['from']['id']
+    
+    if 'text' in msg:
+        msg['text'] = str(msg['text']) # FEELING SAFER ;)
+        msg['type'] = 'text'
+
+    elif 'data' in msg:
+        msg['type'] = 'callback'
+        msg['text'] = f"%callback {msg['data']}"
+
     else:
-        update.message.reply_text("""
-            We have detected unusual request from your device.
-You have been banned from the service until further notice,
-        """)
+        msg['type'] = 'nontext'
+        types = ['audio', 'voice', 'document', 'photo',
+                 'video', 'contact', 'location']
 
-def help(update,context):
-    id = update.message.chat.id
-    if(id == 753971038 or id == -1001331327568):
-        update.message.reply_text("""
+        for type in types:
+            if type in msg:
+                msg['text'] = f'%{type}'
+                break
+
+    if 'text' in msg:
+        for entry in regex:
+            if re.match(entry, msg["text"]):
+                matches = re.match(entry, msg["text"]).groups()
+                parser(msg, list(matches))
+                return
+
+
+app = Flask(__name__)
+
+@app.route(f'/{SECRET}', methods=["POST"])
+def webhook():
+    update = request.get_json()
+    if "message" in update:
+        processing(update['message'])
+
+    elif 'callback_query' in update:
+        processing(update['callback_query'])
+
+    return 'OK'
+
+regex = [
+    r'^[!/](start)',
+    r'^[!/](help)',
+    r'^[!/](streams)'
+]
+
+def parser(msg, matches):
+    usr = msg['from']
+
+    if msg['type'] == "text":
+        if matches[0] == 'start':
+            bot.sendMessage(usr['id'],"Welcome! to Football Story Bot")
+            return
+
+        if matches[0] == 'help':
+            bot.sendMessage(usr['id'], """
         The following commands are available:
         /start -> Welcome Message
         /help -> This Message
         /streams -> list of matches
         """)
-    else:
-        update.message.reply_text("""
-            We have detected unusual request from your device.
-You have been banned from the service until further notice,
-        """)
+            return
+        if matches[0] == 'streams':
+            bot.sendMessage(usr['id'],"Searching for available matches")
+            all_matches_name_list = botTel.all_matches_name()
+            link_str=''
+            if(all_matches_name_list==None):
+                bot.sendMessage(usr['id'],"""We are experiencing problem.
+If problem persists then contact admin""")
+                return None
+            for links in all_matches_name_list:
+                link_str+=links+'\n'
+            bot.sendMessage(usr['id'],link_str)
+            bot.sendMessage(usr['id'],"End Of Detected Matches")
 
-
-def streams(update,context):
-    id = update.message.chat.id
-    if(id == 753971038 or id == -1001331327568):
-        update.message.reply_text("Searching for available matches")
-        all_matches_name_list = bot.all_matches_name()
-        link_str=''
-        if(all_matches_name_list==None):
-            update.message.reply_text("We are experiencing problem.Try again later, if problem persists then contact admin")
-            return None
-        for links in all_matches_name_list:
-            link_str+=links+'\n'
-        update.message.reply_text(link_str)
-        update.message.reply_text("End Of Detected Matches")
-    else:
-        update.message.reply_text("""
-            We have detected unusual request from your device.
-You have been banned from the service until further notice,
-        """)
-
-def handle_message(update,context):
-    id = update.message.chat.id
-    if(id == 753971038 or id == -1001331327568):
-        update.message.reply_text("Searching for available streams")
-        link_str=''
-        sending_message=''
-        for chars in update.message.text:
-            if(chars=='@'):
-                break
-            sending_message+=chars
-        individual_link = bot.selected_match(bot.all_matches_name(),sending_message)
-        if(individual_link==None):
-            update.message.reply_text("We are experiencing problem.Try again later, if problem persists then contact admin hai tw")
-            return None
-        if(len(individual_link)==0):
-            update.message.reply_text("No links found. Links are updated 30min before match. If link is not found till Kick Off then contact admin")
-            return None
-        for links in individual_link:
-            link_str+=links+'\n\n'
-        update.message.reply_text(link_str)
-        update.message.reply_text("End Of Streaming Links")
-    else:
-        update.message.reply_text("""
-            We have detected unusual request from your device.
-You have been banned from the service until further notice,
-        """)
-
-updater = telegram.ext.Updater(TOKEN,use_context=True)
-
-disp = updater.dispatcher
-
-disp.add_handler(telegram.ext.CommandHandler("start",start))
-disp.add_handler(telegram.ext.CommandHandler("help",help))
-disp.add_handler(telegram.ext.CommandHandler("streams",streams))
-disp.add_handler(telegram.ext.MessageHandler(telegram.ext.Filters.command,handle_message))
-updater.start_webhook(listen="0.0.0.0",
-                    port=PORT,
-                    url_path=TOKEN,
-                    webhook_url="https://stream-bot.herokuapp.com/" + TOKEN)
+if __name__ == "__main__":
+    app.run()
