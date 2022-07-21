11e803b11701e5e7b4130fbd46728ec63589a27e
SagunDevkota <sagundevyt@gmail.com>
2022-07-21 23:03:22 +0545

    -

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import bs4
 def all_matches_name():
     try: 
         res = requests.get("https://totalsportek.pro/football/")
-        soup = bs4.BeautifulSoup(res.text,'lxml').find_all('div',attrs={"class":"top-tournament"})
+        soup = bs4.BeautifulSoup(res.text,'html.parser').find_all('div',attrs={"class":"top-tournament"})
 
         all_match_link = []
         all_match_name = []
