9a39233aa7b292ff71d579b7d8f621cd40e305eb
SagunDevkota <sagundevyt@gmail.com>
2022-07-21 16:06:51 +0545

    Updated bot according to website update

--- bot.py
+++ bot.py
@@ -2,25 +2,22 @@ import requests
 import bs4
 def all_matches_name():
     try: 
-        res = requests.get("https://totalsportek.pro/")
-
-        soup = bs4.BeautifulSoup(res.text,'lxml').find_all('div',attrs={"class":"div-main-box"})
+        res = requests.get("https://totalsportek.pro/football/")
+        soup = bs4.BeautifulSoup(res.text,'lxml').find_all('div',attrs={"class":"top-tournament"})
 
         all_match_link = []
         all_match_name = []
         for league in soup:
             links = league.find_all("a")
             for link in links:
-                temp = link.find_all("span",attrs={"class":"mt-2"})
-                home = ""
-                away = ""
-                for chars in temp[0].contents[0].upper():
+                temp = link["title"]
+                matchName = ""
+                for chars in temp.upper():
                     if(ord(chars)>=65 and ord(chars)<=90):
-                        home+=chars
-                for chars in temp[1].contents[0].upper():
-                    if(ord(chars)>=65 and ord(chars)<=90):
-                        away+=chars
-                all_match_name.append((f'/{home}vs{away}').replace(" ",""))
+                        matchName+=chars
+                    if(ord(chars) == ord(' ')):
+                        matchName+='_'
+                all_match_name.append((f'/{matchName}'))
                 all_match_link.append(link['href'])
 
 
@@ -36,11 +33,14 @@ def selected_match(all_matches,user_match_name):
         all_links = 0
         users_match = requests.get(all_matches[user_match_name])
 
-        all_contents = bs4.BeautifulSoup(users_match.text,'lxml').find_all("tr",attrs={"class":"rounded-pill"})
+        all_contents = bs4.BeautifulSoup(users_match.text,'lxml').find_all("tr")
         all_individual_match_link = []
         for content in all_contents:
+            link_content = content.find_all("a")
+            if(len(link_content) == 0):
+                continue
             all_links = all_links+1
-            all_individual_match_link.append(content.find_all("input")[0]['value'])
+            all_individual_match_link.append(link_content[0]['href'])
             if(all_links>=15):
                 break
         return all_individual_match_link
