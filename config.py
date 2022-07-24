aa196d6da9a2deb6d35453eeb42b9e71ad61d1ea
SagunDevkota <sagundevyt@gmail.com>
2022-07-24 19:07:09 +0545

    list

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 TOKEN = os.environ['TOKEN']
 PORT = int(os.environ.get('PORT',5000))
 BASE_URL = os.environ['BASE_URL']
-USER1 = os.environ['user1']
-USER2 = os.environ['user2']
-TEST_USER = os.environ['testUser']
+USER1 = int(os.environ['user1'])
+USER2 = int(os.environ['user2'])
+TEST_USER = int(os.environ['testUser'])
